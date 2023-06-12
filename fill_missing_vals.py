ba6bdaada2b97bbd0f1452badcc06e8ae3182896
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-06-12 16:23:26 -0400

    230612

--- fill_missing_vals.py
+++ fill_missing_vals.py
@@ -7,6 +7,14 @@ import pandas as pd
 
 df = pd.read_csv('/path/to/csv')
 df_c=df #make copy of df
+
+#loop option
+loop_cols = df_c.loc[:, df_c.columns!='grp']    #loop all cols except grouping variable
+col_names = [col for col in loop_cols]
+for col in col_names:
+    df_c[col] = df_c[col].fillna(df_c.groupby('grp')[col].transform('median'))      #transform according to group median
+
+#single column option
 df_c['<column name>'] = df_c['<column name>'].fillna(df_c.groupby('<grouping variable>')['column name'].transform('median'))
 df_c['<column name>'] = df_c['<column name>'].fillna(df_c.groupby('<grouping variable>')['column name'].transform('mean'))
 
