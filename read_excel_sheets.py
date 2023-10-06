66acecd5d4fb7c97e51f0c2edf9fbc582130a6ee
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-10-06 08:35:47 -0400

    231006

--- read_excel_sheets.py
+++ read_excel_sheets.py
@@ -1,6 +1,28 @@
 '''Module to read separate sheets in excel file'''
+
+#------option 1 | manual entry-----
+import pandas as pd
 pd.set_option('display.max_columns', None)
 file1, file2 = None, None
 with pd.ExcelFile(<path/to/file> + '<infile>') as reader:
     file1 = pd.read_excel(reader, sheet_name='<sheet name>')
     file2 = pd.read_excel(reader, sheet_name='<sheet name>')
+
+
+#------option 2-----
+'''function to read single sheet'''
+import pandas as pd
+pd.set_option('display.max_columns', None)
+infile_name=input('Enter path/to/infile.xlsx\n> ');
+def excel_reader(infile):
+    df = pd.read_excel(
+        io=infile,
+        engine="openpyxl",
+        #sheet_name=sheet,
+        #nrows=1000
+    )
+    return df
+df = excel_reader(infile_name)
+print(df.info())
+
+
