82f0260c6ffc1c7d5ad1fcc678be2a9eacbb9c34
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-10-27 11:17:56 -0400

    231027

---
+++ logger.py
@@ -0,0 +1,21 @@
+import logging
+
+def configure_logger():
+    """ Configures logger used throughout the application
+        To get an instance of the configured logger from any module, simply call:
+            `logger = logging.getLogger(__name__)`
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+        handlers=[
+            logging.FileHandler("out.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+#configure_logger
+configure_logger()
+logger = logging.getLogger(__name__)
+logger.info("Starting code")
+logger.info("Ending code")
