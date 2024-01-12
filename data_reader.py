69bd40b91266789a909c179c5781ef8f89f4e363
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2024-01-12 13:12:17 -0500

    240112

--- data_reader.py
+++ data_reader.py
@@ -21,3 +21,24 @@ def data_reader(fname):
 if __name__ == '__main__':
     path = file_path()
     df = data_reader()
+
+
+
+## -------------- option 2 --------------
+# configure directories
+class directories:
+    '''Should be accessible in each module'''
+    base_dir = '<infile>' 
+    data_dir = os.path.join(base_dir, '<additional path>/')
+
+# define infile
+infile='<infile>'
+
+# read file
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+#file1, file2 = None, None
+#with pd.ExcelFile(<path/to/file> + '<infile>') as reader:
+    #file1 = pd.read_excel(reader, sheet_name='<sheet name>')
+    #file2 = pd.read_excel(reader, sheet_name='<sheet name>')
+df = pd.read_csv(directories.data_dir + infile)
--- set_directory_class.py
+++ set_directory_class.py
@@ -4,8 +4,8 @@
 import os
 class directories:
     '''Should be accessible in each module'''
-    base_dir = '/Users/jessedesimone/Documents/' 
-    sub_dir = os.path.join(base_dir, 'postdoc_uf/')
+    base_dir = '<base directory>' 
+    sub_dir = os.path.join(base_dir, '<sub-directory>')
 
 print("Listing directory: {}".format(directories.sub_dir))
 print(os.listdir(directories.sub_dir))
